6d3344dbb4c76756c8ee0b21802e73d50f1ea8b3
AlisonMarques <alison-marques@outlook.com.br>
2020-12-03 18:04:09 -0300

    initial commit

---
+++ config/metadata.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `Refrigeração Marques`,
+    siteUrl: `https://www.gatsbyjs.com`,
+    description: `O melhor em assistência técnica em refrigeração`,
+    social: [
+      {
+        name: "facebook",
+        url: "https://www.facebook.com/alison.marquesdejesus",
+      },
+    ],
+  },
+  pathPrefix: "/assistencia",
+}
